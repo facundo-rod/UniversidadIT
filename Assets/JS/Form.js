134ae69b226a2671708de0be9c2522858300f01b
facundo-rod <facundo.rodr95@gmail.com>
2024-06-22 17:49:21 -0300

    first commit

--- Assets/JS/Form.js
+++ Assets/JS/Form.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     var form = document.querySelector("form");
     form.addEventListener("submit", function(event) {
-        event.preventDefault(); // Previene el envío del formulario
+        event.preventDefault(); // Previene el envio del formulario
         console.log("Formulario enviado");
         alert("Formulario enviado exitosamente");
-        form.submit(); // Envía el formulario después de mostrar la alerta
+        form.submit(); // Envia el formulario despues de mostrar la alerta
     });
 });
